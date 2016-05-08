40dc8243c90e01a2bc951a2e09171a0814dd41f6
Forest5577 <Forest5757@gmail.com>
2016-05-08 23:32:22 +0800

    The Original Version of The Program
    The first version of program first been done.

---
+++ NumberPrecise.java
@@ -0,0 +1,46 @@
+//import API
+import java.math.*;
+
+public class NumberPrecise{
+  int scale = 0;
+
+  BigDecimal number1;
+  BigDecimal number2;
+
+  public NumberPrecise(){
+
+  }
+
+  public NumberPrecise(String number1, String number2){
+    setNumber1(number1);
+    setNumber2(number2);
+  }
+
+  public void setNumber1(String number){
+    this.number1 = new BigDecimal(number);
+  }
+
+  public void setNumber2(String number){
+    this.number2 = new BigDecimal(number);
+  }
+
+  public void setdecimalpoint(int decimalpoint){
+    this.decimalpoint = decimalpoint;
+  }
+
+  public void addNumber(){
+    System.out.println(number1.add(number2));
+  }
+
+  public void subtractNumber(){
+    System.out.println(number1.subtract(number2));
+  }
+
+  public void multiplyNumber(){
+    System.out.println(number1.multiply(number2));
+  }
+
+  public void divideNumber(){
+    System.out.println(number1.divide(number2,decimalpoint,BigDecimal.ROUND_HALF_UP));
+  }
+}
---
+++ TestNumberPrecise.java
@@ -0,0 +1,62 @@
+//import API
+import java.util.Scanner;
+
+public class TestNumberPrecise{
+
+  public static void main(String[] args){
+
+      Scanner scanner = new Scanner(System.in);
+
+      System.out.print("\nPlease Enter The First Number You Want To Calculate:");
+      String number1 = scanner.next();
+      System.out.print("\nPlease Enter The First Number You Want To Calculate:");
+      String number2 = scanner.next();
+
+      NumberPrecise numberPrecise = new NumberPrecise(number1,number2);
+
+      System.out.print("\nPlease choose what function you want to use:\n(a)Add (b)Subtract (c)Multiply (d)Divide");
+      Char function = input.next();
+
+      switch(function){
+        case 'a':
+          numberPrecise.addNumber();
+          break;
+        case 'A':
+          numberPrecise.addNumber();
+          break;
+        case 'b':
+          numberPrecise.subtractNumber();
+          break;
+        case 'B':
+          numberPrecise.subtractNumber();
+          break;
+        case 'c':
+          numberPrecise.multiplyNumber();
+          break;
+        case 'C':
+          numberPrecise.multiplyNumber();
+          break;
+        case 'd':
+          System.out.print("\nHow many digit did you want to know after Decimal Point?");
+          int decimalpoint = input.nextInt();
+          if(decimalpoint<=0)
+            numberPrecise.divideNumber();
+          else{
+            numberPrecise.setdecimalpoint(decimalpoint);
+            numberPrecise.divideNumber();
+          }
+          break;
+        case 'D':
+          System.out.print("\nHow many digit did you want to know after Decimal Point?");
+          int decimalpoint = input.nextInt();
+          if(decimalpoint<=0)
+            numberPrecise.divideNumber();
+          else{
+            numberPrecise.setdecimalpoint(decimalpoint);
+            numberPrecise.divideNumber();
+          }
+          break;
+        default:
+          System.out.println("\nError input.");
+  }
+}
